f643c79cfc743d4cef403f61f9300026a9403a3d
sohae <lidomex@gmail.com>
2021-09-16 15:04:24 +0900

    add nodemon

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+const {User} = require("./model/User")
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 mongoose
   .connect(
     "mongodb+srv://sohae:sohae123@cluster0.gl0k0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   )
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
+
+app.post('/register', (req, res) =>{
+  // 회원가입 시 필요한 정보들을 client에서 가져오면
+  // 그것들을 database에 넣어준다
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({success: true})
+  })
+  
+})
+
+
 
 // root directory에 오면 hello world 출력
 app.get("/", (req, res) => res.send("Hello World!"));
