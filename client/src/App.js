dc57a1a0e9f925fda2f347abdee41a8c684ab0e5
sohae <lidomex@gmail.com>
2021-09-18 17:42:12 +0900

    add components - view pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import logo from "./logo.svg";
+import {BrowserRouter as Router, Switch, Link, Route }from 'react-router-dom';
 import "./App.css";
 
+import Join from './components/view/Join/Join'
+import LandingPage from './components/view/LandingPage/LandingPage.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/join"><Join/></Route>
+          <Route path="/calendar"></Route>
+          <Route path="/mypage">
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
