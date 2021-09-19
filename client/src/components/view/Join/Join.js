3ae8cf4247fbce503a5015b03f50922e731426bd
sohae <lidomex@gmail.com>
2021-09-20 01:15:48 +0900

    add password validation

--- client/src/components/view/Join/Join.js
+++ client/src/components/view/Join/Join.js
@@ -1,7 +1,86 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 
-function Join() {
-  return <div></div>;
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_actions";
+
+function Join(props) {
+  const dispatch = useDispatch();
+  // 안에서 데이터 변화를 시키기 위해서는 state
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [PasswordCheck, setPasswordCheck] = useState("");
+  const [Name, setName] = useState("");
+  const [Birth, setBrith] = useState("");
+  let [passwordChecking, setpasswordChecking] = useState(false);
+
+  const onEmailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+  const onPasswordHandler = (event) => {
+    console.log(event.target.value);
+    setPassword(event.target.value);
+    console.log("password:", Password);
+  };
+  const onPasswordCheckHandler = (event) => {
+    setPasswordCheck(event.currentTarget.value);
+    console.log("Chcek:", PasswordCheck);
+  };
+
+  useEffect(() => {
+    if (Password !== "" && Password === PasswordCheck) {
+      setpasswordChecking(true);
+    } else {
+      setpasswordChecking(false);
+    }
+  }, [PasswordCheck, Password]);
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    alert(Email);
+    alert(Password);
+
+    let body = {
+      email: Email,
+      password: Password,
+    };
+  };
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={onSubmitHandler}
+        >
+          <label>Email</label>
+          <input type="email" value={Email} onChange={onEmailHandler} />
+          <label> Password </label>
+          <input
+            type="password"
+            value={Password}
+            onChange={onPasswordHandler}
+          />
+          <label> Password Check </label>
+          <input
+            type="password"
+            value={PasswordCheck}
+            onChange={onPasswordCheckHandler}
+          />
+          {passwordChecking
+            ? "비밀번호가 일치합니다"
+            : "비밀번호가 일치하지 않습니다"}
+          <span></span>
+          <button>Login</button>
+        </form>
+      </div>
+      );
+    </div>
+  );
 }
 
 export default Join;
