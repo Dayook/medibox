4364710a17cac218979b3ae78fd943a9a6e38eba
sohae <lidomex@gmail.com>
2021-09-22 23:44:13 +0900

    update nav

--- client/src/components/view/NavBar/NavBar.js
+++ client/src/components/view/NavBar/NavBar.js
@@ -22,36 +22,38 @@ function NavBar() {
       className="menu"
       style={{ position: "fixed", zIndex: 5, width: "100%" }}
     >
-      <div className="menu__logo">
-        <a href="/">
-          <img src={logo} alt="logo" style={{ width: "100px" }}></img>
-        </a>
-      </div>
-      <div className="menu__container">
-        <div className="menu_left">
-          <LeftMenu mode="horizontal" />
+      <div className="nav__container">
+        <div className="menu__logo">
+          <a href="/">
+            <img src={logo} alt="logo" style={{ width: "100px" }}></img>
+          </a>
         </div>
-        <div className="menu_right">
-          <RightMenu mode="horizontal" />
+        <div className="menu__container">
+          <div className="menu_left">
+            <LeftMenu mode="horizontal" />
+          </div>
+          <div className="menu_right">
+            <RightMenu mode="horizontal" />
+          </div>
+          <Button
+            className="menu__mobile-button"
+            type="primary"
+            onClick={showDrawer}
+          >
+            <Icon type="align-right" />
+          </Button>
+          <Drawer
+            title="Basic Drawer"
+            placement="right"
+            className="menu_drawer"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <LeftMenu mode="inline" />
+            <RightMenu mode="inline" />
+          </Drawer>
         </div>
-        <Button
-          className="menu__mobile-button"
-          type="primary"
-          onClick={showDrawer}
-        >
-          <Icon type="align-right" />
-        </Button>
-        <Drawer
-          title="Basic Drawer"
-          placement="right"
-          className="menu_drawer"
-          closable={false}
-          onClose={onClose}
-          visible={visible}
-        >
-          <LeftMenu mode="inline" />
-          <RightMenu mode="inline" />
-        </Drawer>
       </div>
     </nav>
   );
