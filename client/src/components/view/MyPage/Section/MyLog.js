10ebf8c5b9cf3a14a73a95215a7c43a1948cbfc5
sohae <lidomex@gmail.com>
2021-09-23 23:05:16 +0900

    update mylog template

--- client/src/components/view/MyPage/Section/MyLog.js
+++ client/src/components/view/MyPage/Section/MyLog.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import Icon from "@ant-design/icons";
 
 function MyLog() {
   return (
-    <div>
-      
+    <div
+      style={{
+        margin: "0 auto",
+        padding: "100px 0",
+      }}
+    >
+      <div
+        style={{
+          width: "700px",
+          borderRadius: "10px",
+          border: "1px solid black",
+          margin: "0px auto",
+          padding: "30px",
+        }}
+      >
+        나의 약상자
+        <div
+          style={{
+            width: "180px",
+            height: "180px",
+            display: "flex",
+            border: "1px solid #dedede",
+            color: "#2e2e2e",
+          }}
+        >
+          <Icon type="plus" style={{ fontSize: "3rem" }} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MyLog
+export default MyLog;
--- client/src/components/view/MyPage/Section/MyMedicine.js
+++ client/src/components/view/MyPage/Section/MyMedicine.js
@@ -5,7 +5,7 @@ import MyLog from "./MyLog";
 function MyMedicine() {
   return (
     <div className="container" style={{ width: "1100px", margin: "0 auto" }}>
-      <MyLog />
+      <MyLog style={{ padding: " 100px" }} />
       <MyCalendar style={{ padding: "50px 0px", width: "768px" }}></MyCalendar>
     </div>
   );
