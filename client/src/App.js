670f8a03370787f7f9f8f050470f1442d3c8e7b2
sohae <lidomex@gmail.com>
2021-09-25 00:17:44 +0900

    update landing page css

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import NavBar from "./components/view/NavBar/NavBar";
 import Auth from "./hoc/auth";
 import MyMedicine from "./components/view/MyPage/Section/MyMedicine";
 import Counsel from "./components/view/Counsel/Counsel";
+import Medicine from "./components/view/Medicine/Medicine";
+import Footer from "./components/view/Footer/Footer";
 
 function App() {
   return (
@@ -24,6 +26,7 @@ function App() {
             <Route exact path="/login" component={Auth(Login, false)} />
             <Route exact path="/register" component={Auth(Join, false)} />
             <Route exact path="/mypage" component={Auth(MyPage, true)} />
+            <Route exact path="/medicine" component={Auth(Medicine)} />
             <Route
               exact
               path="/my-medicine"
@@ -38,6 +41,7 @@ function App() {
           </Switch>
         </div>
       </Router>
+      <Footer />
     </Suspense>
   );
 }
