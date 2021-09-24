4ac2b7c5a2f763b4a0e8ed251f4b731ca3fbf482
sohae <lidomex@gmail.com>
2021-09-24 23:23:35 +0900

    update footer

---
+++ client/src/components/view/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div
+      style={{
+        height: "120px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "1rem",
+        backgroundColor: "#242424",
+        color: "aliceblue",
+        padding: "20px",
+      }}
+    >
+      <p>©Sohae Kim</p>
+    </div>
+  );
+}
+
+export default Footer;
