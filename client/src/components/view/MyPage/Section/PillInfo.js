b07751b525484bc643bee9afedfd17739c249cdc
sohae <lidomex@gmail.com>
2021-09-27 02:45:13 +0900

    update load image

--- client/src/components/view/MyPage/Section/PillInfo.js
+++ client/src/components/view/MyPage/Section/PillInfo.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function PillInfo(props) {
+  const img_src =
+    "https://www.pharm.or.kr:442/images/sb_photo/big3/" +
+    props.insert_file.substr(-20, 13) +
+    "01.jpg";
   if (props.ITEM_NAME === "") {
     return <div>ㅎㅎ</div>;
   } else {
@@ -9,6 +13,11 @@ function PillInfo(props) {
         {" "}
         {props.item_name} <br></br>
         {props.insert_file}
+        <img
+          src={img_src}
+          style={{ width: "-webkit-fill-available" }}
+          alt="medicine_image"
+        />
         {props.storage_method}
       </div>
     );
